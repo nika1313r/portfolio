46441b141021173d3f7bb98f892de7107dada34a
nika1313r <147719211+nika1313r@users.noreply.github.com>
2024-01-30 16:45:56 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let button = document.querySelector(".main-button");
-    let wrapper = document.querySelector(".wrapper");
+    document.querySelector(".menu").addEventListener("click",()=>(
+        document.querySelector(".nav-bar").classList.add("active")
+    ))
+    document.querySelector(".close").addEventListener("click",()=>(
+        document.querySelector(".nav-bar").classList.remove("active")
+    ))
+    document.querySelectorAll(".element").forEach((element) =>{
+        element.addEventListener("click",()=>(
+            document.querySelector(".nav-bar").classList.remove("active")
+        ))
+    })
 
-    button.onclick = function() {
-        wrapper.classList.toggle("active");
-    }
+    document.querySelectorAll(".element").forEach((element, index) => {
+        element.addEventListener("click", () => {
+            if (index === 0) {
+                window.scrollTo(0, 0);
+            } else if (index === 1) {
+                window.scrollTo(0, window.innerHeight)
+            } else if (index === 2) {
+                window.scrollTo(0, window.innerHeight * 2)
+            } else if (index === 3) {
+                window.scrollTo(0, window.innerHeight * 3)
+            }
+        })
+    })
+
 });
